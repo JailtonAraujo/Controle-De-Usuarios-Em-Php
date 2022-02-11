118ce3633d5f3ea7ee76329484c89f09178bdf13
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-02-10 22:37:19 -0300

    update

--- src_sql/sql_src.sql
+++ src_sql/sql_src.sql
@@ -6,9 +6,10 @@ USE `sistema-php`;
 -- TABLE DE USUARIO --
 create table if not exists `usuario`(
 	idusuario int(11) not null auto_increment,
-    nome varchar(120) not null,
-    login varchar (255) not null,
-    senha varchar (255) not null,
+    nome varchar(60) not null,
+    login varchar (40) not null,
+	email varchar (120) not null,
+    senha varchar (30) not null,
     primary key(idusuario),
     unique index `login_UNIQUE` (`login` asc) visible,
     unique index `senha_UNIQUE` (`senha` asc) visible
@@ -16,11 +17,25 @@ create table if not exists `usuario`(
 default character set = utf8;
 
 -- QUERY DE INSERT NO BANCO --
-insert into usuario (nome, login, senha) values ('admin', 'admin', 'admin');
+insert into usuario (nome, login, senha, email) values ('?', '?', '?', '?');
 
 --  QUERY DE CONSULTA NO BANCO --
 select * from usuario;
 
 -- QUERY DE LOGIN --
-select login, idusuario from usuario where senha = md5('admin') and login = 'admin'
+select login, idusuario from usuario where senha = '?' and login = '?';
 
+-- QUERY DE DELETE --
+delete from usuario where idusuario = ?;
+
+-- QUERY DE BUSCA POR NOME --
+select * from usuario where nome like upper('?');
+
+-- QUERY DE UPDATE --
+update usuario set nome = '?', login = '?', email = '?', senha = '?' where idusuario = ?;
+
+-- QUERY DE BUSCAR PO ID -- 
+select * from usuario where idusuario = ?;
+
+-- QUERY DE CONTAGEM --
+select count(*) from usuario;  
